fc4fbc02e458f93f30166dcc5e21a82ef6f47a5e
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-11-23 16:28:31 +0900

    Add lambchop-dump

--- lib/lambchop.rb
+++ lib/lambchop.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 require 'yaml'
 
 require 'aws-sdk-core'
@@ -6,5 +7,6 @@ require 'zip'
 module Lambchop; end
 
 require 'lambchop/client'
+require 'lambchop/dump'
 require 'lambchop/logs_watch_dog'
 require 'lambchop/version'
---
+++ lib/lambchop/dump.rb
@@ -0,0 +1,52 @@
+class Lambchop::Dump
+  def self.dump(function_name, options = {})
+    self.new(function_name, options).dump
+  end
+
+  def initialize(function_name, options = {})
+    @function_name = function_name
+    @client        = options[:client] || Aws::Lambda::Client.new(region: 'us-east-1')
+    @out           = options[:out] || $stdout
+  end
+
+  def dump
+    page = @client.get_function(:function_name => @function_name).first
+    puts_shebang
+    puts_magic_comment(page.configuration)
+    puts_source(page.code.location)
+  end
+
+  def puts_shebang
+    @out.puts('#!/usr/bin/env lambchop')
+  end
+
+  def puts_magic_comment(configuration)
+    comment_attrs = {}
+
+    %w(
+      function_name
+      runtime
+      role
+      handler
+      mode
+      description
+      timeout
+      memory_size
+    ).each do |name|
+      comment_attrs[name] = configuration.send(name)
+    end
+
+    yaml = YAML.dump(comment_attrs).sub(/\A---\n/, '')
+    @out.puts("/*\n#{yaml}*/")
+  end
+
+  def puts_source(location)
+    open(location) do |f|
+      Zip::InputStream.open(f) do |zis|
+        while entry = zis.get_next_entry
+          @out.puts(entry.get_input_stream.read)
+        end
+      end
+    end
+  end
+end
